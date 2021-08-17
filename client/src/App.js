3a227b228dbbbf845aa3fc032c70ae02ce5eafd5
Haojie <alanchenbaby123@gmail.com>
2021-08-17 23:58:08 +0930

    post

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import 'semantic-ui-css/semantic.min.css';
+import './App.css';
+import { AuthProvider } from './context/auth'
+import AuthRoute from './util/AuthRoute'
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import MenuBar from './components/MenuBar';
+
+function App(){
+  return(
+    <AuthProvider>
+      <Router>
+        <div className="ui container">
+          <MenuBar />
+          <Route exact path='/' component={Home} />
+          <AuthRoute exact path='/login' component={Login} />
+          <AuthRoute exact path='/register' component={Register} />
+        </div>
+      </Router>
+    </AuthProvider>
+  )
+}
+export default App;
